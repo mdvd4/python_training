cbcced450ddbc704a034128afa7dd87d05639e59
lavr <mdvd4@mail.ru>
2019-07-19 14:47:21 +0300

    Улучшение методов login, create group

--- test_add_group.py
+++ test_add_group.py
@@ -11,11 +11,8 @@ class TestAddGroup(unittest.TestCase):
 
     def test_add_group(self):
         wd = self.wd
-        self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
-        self.open_group_page(wd)
         self.create_group(wd, Group(name="Group name", header="Group header", footer="Group footer"))
-        self.return_groups_page(wd)
         self.logout(wd)
 
     def test_add_empty_group(self):
@@ -35,6 +32,7 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_link_text("groups").click()
 
     def create_group(self, wd, group):
+        self.open_group_page(wd)
         # init group creation
         wd.find_element_by_name("new").click()
         # fill group form
@@ -47,11 +45,13 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_name("group_footer").send_keys(group.footer)
         # submit group creation
         wd.find_element_by_name("submit").click()
+        self.return_groups_page(wd)
 
     def open_group_page(self, wd):
         wd.find_element_by_link_text("groups").click()
 
     def login(self, wd, username, password):
+        self.open_home_page(wd)
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys(username)
         wd.find_element_by_name("pass").clear()
