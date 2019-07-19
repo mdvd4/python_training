df3460fd88cf0453855ba5de2531150c083d87a9
lavr <mdvd4@mail.ru>
2019-07-19 21:26:18 +0300

    Добавлен тест на редактирование группы, вынесен метод на заполнение полей группы

--- fixture/group.py
+++ fixture/group.py
@@ -13,14 +13,7 @@ class GroupHelper:
         self.open_group_page()
         # init group creation
         wd.find_element_by_name("new").click()
-        # fill group form
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        self.fill_group_form(group)
         # submit group creation
         wd.find_element_by_name("submit").click()
         self.return_groups_page()
@@ -37,3 +30,27 @@ class GroupHelper:
         wd.find_element_by_name("selected[]").click()
         # submit_deletion
         wd.find_element_by_name("delete").click()
+        self.return_groups_page()
+
+    def edit_first_group(self, group):
+        wd = self.app.wd
+        self.open_group_page()
+        # select_first_group
+        wd.find_element_by_name("selected[]").click()
+        # submit edit group
+        wd.find_element_by_name("edit").click()
+        self.fill_group_form(group)
+        # submit_update
+        wd.find_element_by_name("update").click()
+        self.return_groups_page()
+
+    def fill_group_form(self, group):
+        wd = self.app.wd
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys(group.name)
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys(group.header)
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
+
