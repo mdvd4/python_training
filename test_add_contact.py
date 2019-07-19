939ce739908bc54795b4b4cb571fde05a371b67d
lavr <mdvd4@mail.ru>
2019-07-19 13:20:27 +0300

    Добавлен тест пустой контакт

--- test_add_contact.py
+++ test_add_contact.py
@@ -15,13 +15,35 @@ class TestAddContact(unittest.TestCase):
         self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
         self.contact_creation(wd)
-        self.create_contact(wd, Contact(firstname="John", middlename="Patrick", lastname="Dow", nickname="JohnDow", title="someTitle", company="someCompany", adress="someAdress",
-                                        phone_home="+1(123)456-78-90", phone_mobile="+1(123)456-78-90", phone_work="+1(123)456-78-90", fax="+1(123)456-78-90",
-                                        email="someEmai.@mail.com", emal2="someEmai.@mail.com", email3="someEmai.@mail.com", homepage="http://someUrl.com",
-                                        bday="1", bmonth="January", byear="1950", aday="1", amonth="January", ayear="1960", group="[none]", adresess2="someAdress", phone2="+1(123)456-78-90",
+        self.create_contact(wd, Contact(firstname="John", middlename="Patrick", lastname="Dow", nickname="JohnDow",
+                                        title="someTitle", company="someCompany", adress="someAdress",
+                                        phone_home="+1(123)456-78-90", phone_mobile="+1(123)456-78-90",
+                                        phone_work="+1(123)456-78-90", fax="+1(123)456-78-90",
+                                        email="someEmai.@mail.com", emal2="someEmai.@mail.com",
+                                        email3="someEmai.@mail.com", homepage="http://someUrl.com",
+                                        bday="1", bmonth="January", byear="1950", aday="1", amonth="January",
+                                        ayear="1960", group="[none]", adresess2="someAdress", phone2="+1(123)456-78-90",
                                         notes="someNotes"))
         self.return_home_page(wd)
         self.logout(wd)
+
+    def test_add_empty_contact(self):
+            wd = self.wd
+            self.open_home_page(wd)
+            self.login(wd, username="admin", password="secret")
+            self.contact_creation(wd)
+            self.create_contact(wd, Contact(firstname="", middlename="", lastname="", nickname="",
+                                            title="", company="", adress="",
+                                            phone_home="", phone_mobile="",
+                                            phone_work="", fax="",
+                                            email="", emal2="",
+                                            email3="", homepage="",
+                                            bday="", bmonth="-", byear="", aday="", amonth="-",
+                                            ayear="", group="[none]", adresess2="",
+                                            phone2="",
+                                            notes=""))
+            self.return_home_page(wd)
+            self.logout(wd)
 
     def create_contact(self, wd, contact):
         # fill contact form
