d1a2d06f2334877d8e790dc6fd0cd2108848528e
lavr <mdvd4@mail.ru>
2019-07-24 10:53:38 +0300

    Доавлен удаленный импорт

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -1,3 +1,6 @@
+from model.group import Group
+
+
 def test_delete_first_group(app):
     app.session.login(username="admin", password="secret")
     app.group.create(Group(name="Group name", header="Group header", footer="Group footer"))
