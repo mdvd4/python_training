a94c5aba96cef3cdb24a109e33b676c0fefe224f
lavr <mdvd4@mail.ru>
2019-10-17 16:50:35 +0300

    Чтение из сгененрированного файла

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,12 +1,13 @@
 import pytest
 
 # from data.add_group import testdata
-from data.add_group import constant as testdata
+# from data.groups import constant as testdata
 from model.group import Group
 
 
-@pytest.mark.parametrize('group', testdata, ids=[repr(x) for x in testdata])
-def test_add_group(app, group):
+# @pytest.mark.parametrize('group', testdata, ids=[repr(x) for x in testdata])
+def test_add_group(app, json_groups):
+    group = json_groups
     old_groups = app.group.get_group_list()
     app.group.create(group)
     assert len(old_groups) + 1 == app.group.count()
@@ -20,5 +21,15 @@ def test_add_group(app, group):
 #     app.group.create(group)
 #     new_groups = app.group.get_group_list()
 #     assert len(old_groups) + 1 == len(new_groups)
+#     old_groups.append(group)
+#     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+
+
+# def test_add_group(app, data_groups):
+#     group = data_groups
+#     old_groups = app.group.get_group_list()
+#     app.group.create(group)
+#     assert len(old_groups) + 1 == app.group.count()
+#     new_groups = app.group.get_group_list()
 #     old_groups.append(group)
 #     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
